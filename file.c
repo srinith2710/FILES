2464f736bd0accd7672aa5d3557da78ea5dd88c9
srinith2710 <103512756+srinith2710@users.noreply.github.com>
2022-05-01 11:09:47 +0530

    file.c

---
+++ file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+ 
+void reverseContent(char* x)
+{
+    FILE* fp = fopen(x, "a+");
+    if (fp == NULL) {
+        printf("Unable to open file\n");
+        return;
+    }
+    char buf[100];
+    int a[MAX], s = 0, c = 0, l;
+    fprintf(fp, " \n");
+    rewind(fp);
+    while (!feof(fp)) {
+        fgets(buf, sizeof(buf), fp);
+        l = strlen(buf);
+        a = s += l;
+    }
+    rewind(fp);
+    c -= 1;
+    while (c >= 0) {
+        fseek(fp, a, 0);
+        fgets(buf, sizeof(buf), fp);
+        printf("%s", buf);
+        c--;
+    }
+ 
+    return ;
+}
+
+int main()
+{
+    char x[] = "file1.txt";
+    FILE *f;
+    f=fopen("file2.txt","r");
+    reverseContent(x);
+    fputc(x,f);
+    fclose(f);
+    return 0;
+}
